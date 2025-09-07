a4fc0716e1bee60371186ca314b9f0be45388721
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2025-09-07 23:06:57 +0200

    Update

--- mvim/.config/mvim/init.lua
+++ mvim/.config/mvim/init.lua
@@ -23,6 +23,7 @@ vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to de
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.scrolloff = 10
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 --- Packages
 vim.pack.add({
@@ -36,23 +37,41 @@ vim.pack.add({
 	{ src = "https://github.com/Saghen/blink.cmp" },             --- Autocomplete
 	{ src = "https://github.com/rafamadriz/friendly-snippets" },
 	{ src = "https://github.com/romgrk/fzy-lua-native" },
-	{ src = "https://github.com/windwp/nvim-autopairs" },                   --- Auto brackets
-	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },     --- Indent
-	{ src = "https://github.com/nvim-mini/mini.statusline" },               --- statusline
-	{ src = "https://github.com/kevinhwang91/nvim-ufo" },                   --- folding
+	{ src = "https://github.com/windwp/nvim-autopairs" },                    --- Auto brackets
+	{ src = "https://github.com/nvim-mini/mini.statusline" },                --- statusline
+	{ src = "https://github.com/kevinhwang91/nvim-ufo" },                    --- folding
 	{ src = "https://github.com/kevinhwang91/promise-async" },
-	{ src = "https://github.com/kylechui/nvim-surround" },                  --- surround
-	{ src = "https://github.com/olimorris/codecompanion.nvim" },            --- AI
+	{ src = "https://github.com/kylechui/nvim-surround" },                   --- surround
+	{ src = "https://github.com/olimorris/codecompanion.nvim" },             --- AI
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }, --- markdown
-	{ src = "https://github.com/echasnovski/mini.diff" },                   --- git diff
-	{ src = "https://github.com/AbdelrahmanDwedar/awesome-nvim-colorschemes" } --- color themes
+	{ src = "https://github.com/echasnovski/mini.diff" },                    --- git diff
+	{ src = "https://github.com/AbdelrahmanDwedar/awesome-nvim-colorschemes" }, --- color themes
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },             --- typst preview
+	{ src = "https://github.com/akinsho/toggleterm.nvim" },                  --- terminal
+	{ src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
+	{ src = "https://github.com/rmagatti/auto-session" },                    --- auto sessions
+	{ src = "https://github.com/OXY2DEV/helpview.nvim" },
+	{ src = "https://github.com/folke/snacks.nvim" },                        --- snacks
+	{ src = "https://github.com/christoomey/vim-tmux-navigator.git" },
 })
 
---- Theme
----require "vague".setup({ transparent = true })
----vim.cmd("colorscheme vague")
----vim.cmd(":hi statusline guibg=NONE")
+--- snacks
+require("snacks").setup({
+	input = { enabled = true },
+	scope = { enabled = true },
+	quickfile = { enabled = true },
+	notifier = { enabled = true },
+	picker = {
+		enabled = true,
+		hidden = true,
+	},
+	scroll = { enabled = true },
+	indent = { enalbed = true },
+	statuscolumn = { enabled = true },
+})
+
+--- theme
 require "catppuccin".setup({
 	flavour = "macchiato",
 	transparent_background = true,
@@ -64,9 +83,31 @@ vim.cmd("colorscheme catppuccin")
 
 --- Treesitter
 require "nvim-treesitter.configs".setup({
-	ensure_installed = { "lua", "typescript", "javascript", "vim", "python", "json", "yaml" },
+	ensure_installed = { "lua", "typescript", "javascript", "vim", "python", "json", "yaml", "typst", "vimdoc" },
 	highlight = { enable = true }
 })
+
+--- toggle term
+require("toggleterm").setup({
+	open_mapping = [[<c-t>]],
+	autochdir = true,
+	direction = "float",
+})
+
+require("auto-session").setup()
+
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+	vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 --- markdown
 require "render-markdown".setup({
@@ -79,6 +120,9 @@ require "mini.diff".setup({
 	source = require("mini.diff").gen_source.none(),
 })
 
+--- inline diagnostic fix
+require('tiny-inline-diagnostic').setup()
+
 --- surround
 require("nvim-surround").setup()
 
@@ -88,17 +132,15 @@ require "nvim-tree".setup()
 --- statusline
 require('mini.statusline').setup()
 
+--- typst preview
+require("typst-preview").setup({
+	port = 5555
+})
+
 --- auto_brackets
 require("nvim-autopairs").setup({
 	check_ts = true,
 	enable_check_bracket_line = true,
-})
-
---- indent-blankline
-require("ibl").setup({
-	scope = {
-		enabled = true,
-	},
 })
 
 --- UFO folding
@@ -137,7 +179,7 @@ vim.diagnostic.config({
 
 local capabilities = blink.get_lsp_capabilities()
 
-local servers = { "lua_ls", "ts_ls", "pyright" }
+local servers = { "lua_ls", "ts_ls", "pyright", "tinymist" }
 
 for _, server in ipairs(servers) do
 	lspconfig[server].setup({
@@ -192,29 +234,35 @@ vim.schedule(function()
 end)
 
 --- Keybinds
-vim.keymap.set('n', '<leader>ff', ":FzfLua files<CR>")
-vim.keymap.set('n', '<leader>fb', ":FzfLua buffers<CR>")
-vim.keymap.set('n', '<leader>fg', ":FzfLua live_grep_native<CR>")
-vim.keymap.set('n', '<leader>/', ":FzfLua grep_curbuf<CR>")
-vim.keymap.set('n', '<leader>lb', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>vv", ":vsplit<CR>", { silent = true, desc = "Split vertical" })
-vim.keymap.set("n", "<leader>vs", ":split<CR>", { silent = true, desc = "Split horizontal" })
-vim.keymap.set("c", "<tab>", "<C-z>", { silent = false })
-vim.keymap.set("n", "<C-Tab>", "<C-w><C-w>", {})
-vim.keymap.set("n", "<C-v>", '"+p')
-vim.keymap.set("v", "<C-c>", '"+y')
-vim.keymap.set("n", "<leader>db", ":bdelete<CR>", { silent = true, desc = "Delete buffer" })
-vim.keymap.set("n", "<leader>dw", "<C-w>c", { desc = "Delete window (keep buffer)" })
-vim.keymap.set("n", "<C-h>", "<C-W>h")
-vim.keymap.set("n", "<C-j>", "<C-W>j")
-vim.keymap.set("n", "<C-k>", "<C-W>k")
-vim.keymap.set("n", "<C-l>", "<C-W>l")
-vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle nvim-tree", silent = true })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-vim.keymap.set('n', '<leader>ua', require('ufo').openAllFolds, { desc = "Open all folds" })
-vim.keymap.set('n', '<leader>uc', require('ufo').closeAllFolds, { desc = "Close all folds" })
-vim.keymap.set('n', '<leader>fo', "<cmd>foldopen<cr>", { desc = "Open fold", silent = true })
-vim.keymap.set('n', '<leader>fc', "<cmd>foldclose<cr>", { desc = "Close fold", silent = true })
-vim.keymap.set('n', '<leader>cc', "<cmd>CodeCompanionChat toggle<cr>", { desc = "Toggle AI chat window", silent = true })
+local map = vim.keymap.set
+local snacks = require("snacks")
+
+map('n', '<leader>lb', vim.lsp.buf.format)
+map('n', '<leader>rn', vim.lsp.buf.rename)
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+map("n", "<leader>vv", ":vsplit<CR>", { silent = true, desc = "Split vertical" })
+map("n", "<leader>vs", ":split<CR>", { silent = true, desc = "Split horizontal" })
+map("c", "<tab>", "<C-z>", { silent = false })
+map("n", "<C-Tab>", "<C-w><C-w>", {})
+map("n", "<C-v>", '"+p')
+map("v", "<C-c>", '"+y')
+map("n", "<leader>db", ":bdelete<CR>", { silent = true, desc = "Delete buffer" })
+map("n", "<leader>dw", "<C-w>c", { desc = "Delete window (keep buffer)" })
+map("n", "<C-h>", "<C-W>h")
+map("n", "<C-j>", "<C-W>j")
+map("n", "<C-k>", "<C-W>k")
+map("n", "<C-l>", "<C-W>l")
+map("n", "<leader>n", function() snacks.explorer() end, { desc = "Toggle Explorer", silent = true })
+map("n", "<leader>ff", function() snacks.picker.smart() end, { desc = "Find files", silent = true })
+map("n", "<leader>fb", function() snacks.picker.buffers() end, { desc = "Find buffers", silent = true })
+map("n", "<leader>fg", function() snacks.picker.grep() end, { desc = "Grep", silent = true })
+map("n", "<leader>fn", function() snacks.picker.notifications() end, { desc = "Notifications history", silent = true })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+map('n', '<leader>ua', require('ufo').openAllFolds, { desc = "Open all folds" })
+map('n', '<leader>uc', require('ufo').closeAllFolds, { desc = "Close all folds" })
+map('n', '<leader>fo', "<cmd>foldopen<cr>", { desc = "Open fold", silent = true })
+map('n', '<leader>fc', "<cmd>foldclose<cr>", { desc = "Close fold", silent = true })
+map('n', '<leader>cc', "<cmd>CodeCompanionChat toggle<cr>", { desc = "Toggle AI chat window", silent = true })
+map('n', '<leader>ss', "<cmd>AutoSession save<cr>", { desc = "Save current session", silent = true })
+map('n', '<leader>sf', "<cmd>AutoSession search<cr>", { desc = "Search for session", silent = true })
+map('n', '<leader>st', "<cmd>AutoSession toggle<cr>", { desc = "Toggle auto session", silent = true })
